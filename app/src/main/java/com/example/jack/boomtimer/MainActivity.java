e29ef3dddd1283b9b78c9a65bfc3e36b2ed3dd75
Jack Case <jackacase@gmail.com>
2017-07-11 21:16:43 -0400

    Main Activity updated so picker and radio buttons work as expected. The radio buttons were added to a radio group to handle the mutual exclusivity. The minimum player count of the picker was set to 6, as the minimum number of players for the game. If the number of players is 10 or fewer, only the 3 round option will be available. numPlayers and numRounds are set by the UI controls.

--- app/src/main/java/com/example/jack/boomtimer/MainActivity.java
+++ app/src/main/java/com/example/jack/boomtimer/MainActivity.java
@@ -1,13 +1,88 @@
 package com.example.jack.boomtimer;
 
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.NumberPicker;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int numPlayers;
+    private int numRounds;
+
+    private static final String TAG = "BoomTimer";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* set up the player count number picker */
+        final NumberPicker np_player_picker;
+        np_player_picker = (NumberPicker)findViewById(R.id.player_picker);
+        np_player_picker.setMinValue(6);
+        np_player_picker.setMaxValue(50);
+        np_player_picker.setWrapSelectorWheel(false);
+        /* TODO set the player count on app start to what it previously was */
+
+        /* set up the radio buttons for number of rounds */
+        final RadioGroup rg_rounds;
+        rg_rounds = (RadioGroup)findViewById(R.id.rounds_group);
+        final RadioButton rb_rounds_3;
+        rb_rounds_3 = (RadioButton)findViewById(R.id.rounds_3);
+        final RadioButton rb_rounds_5;
+        rb_rounds_5 = (RadioButton)findViewById(R.id.rounds_5);
+        /* with fewer than 10 players, only 3 rounds are allowed */
+        if(np_player_picker.getValue() <= 10) {
+            rb_rounds_5.setChecked(false);
+            rb_rounds_5.setEnabled(false);
+            rb_rounds_3.setChecked(true);
+            numRounds = 3; //TODO this is not a good place for numRounds to be initialized
+        }
+        /* otherwise 3 or 5 rounds may be selected */
+        /* TODO in this case remember if 3 or 5 rounds was previously selected */
+        else {
+            rb_rounds_5.setEnabled(true);
+            rb_rounds_5.setChecked(false);
+            rb_rounds_3.setChecked(true);
+        }
+        Log.i(TAG, "Rounds = " + numRounds);
+
+        /* set the callback for changing picker value. Set numPlayers and enable or disable 5 rounds */
+        np_player_picker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                numPlayers = newVal;
+                Log.i(TAG, "Players = " + numPlayers);
+                if(newVal <= 10) {
+                    rb_rounds_5.setChecked(false);
+                    rb_rounds_3.setChecked(true);
+                    rb_rounds_5.setEnabled(false);
+                }
+                else {
+                    rb_rounds_5.setEnabled(true);
+                }
+            }
+        });
+
+        /* set the callbacks for the radio buttons. Only one can be activated at a time. */
+        rg_rounds.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
+                if(rb_rounds_3.isChecked()) {
+                    numRounds = 3;
+                    Log.i(TAG, "Rounds = " + numRounds);
+                }
+                else if (rb_rounds_5.isChecked()){
+                    numRounds = 5;
+                    Log.i(TAG, "Rounds = " + numRounds);
+                }
+            }
+        });
     }
+
+
 }
