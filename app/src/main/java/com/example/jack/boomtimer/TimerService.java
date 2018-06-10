b3f80d5d15b7ec666d10f8fbf3e75f8bda0a6d7e
GandalfDaGreat <jackacase@gmail.com>
2018-06-09 23:05:21 -0400

    Getting started implementing the timer service

--- app/src/main/java/com/example/jack/boomtimer/TimerService.java
+++ app/src/main/java/com/example/jack/boomtimer/TimerService.java
@@ -14,12 +14,10 @@ import android.content.Context;
 public class TimerService extends IntentService {
     // TODO: Rename actions, choose action names that describe tasks that this
     // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
-    private static final String ACTION_FOO = "com.example.jack.boomtimer.action.FOO";
-    private static final String ACTION_BAZ = "com.example.jack.boomtimer.action.BAZ";
+    private static final String ACTION_RUN_TIMER = "com.example.jack.boomtimer.action.RUN_TIMER";
 
     // TODO: Rename parameters
-    private static final String EXTRA_PARAM1 = "com.example.jack.boomtimer.extra.PARAM1";
-    private static final String EXTRA_PARAM2 = "com.example.jack.boomtimer.extra.PARAM2";
+    private static final String EXTRA_SECONDS = "com.example.jack.boomtimer.extra.SECONDS";
 
     public TimerService() {
         super("TimerService");
@@ -32,26 +30,10 @@ public class TimerService extends IntentService {
      * @see IntentService
      */
     // TODO: Customize helper method
-    public static void startActionFoo(Context context, String param1, String param2) {
+    public static void startActionRunTimer(Context context, String param1, String param2) {
         Intent intent = new Intent(context, TimerService.class);
-        intent.setAction(ACTION_FOO);
-        intent.putExtra(EXTRA_PARAM1, param1);
-        intent.putExtra(EXTRA_PARAM2, param2);
-        context.startService(intent);
-    }
-
-    /**
-     * Starts this service to perform action Baz with the given parameters. If
-     * the service is already performing a task this action will be queued.
-     *
-     * @see IntentService
-     */
-    // TODO: Customize helper method
-    public static void startActionBaz(Context context, String param1, String param2) {
-        Intent intent = new Intent(context, TimerService.class);
-        intent.setAction(ACTION_BAZ);
-        intent.putExtra(EXTRA_PARAM1, param1);
-        intent.putExtra(EXTRA_PARAM2, param2);
+        intent.setAction(ACTION_RUN_TIMER);
+        intent.putExtra(EXTRA_SECONDS, param1);
         context.startService(intent);
     }
 
@@ -59,14 +41,9 @@ public class TimerService extends IntentService {
     protected void onHandleIntent(Intent intent) {
         if (intent != null) {
             final String action = intent.getAction();
-            if (ACTION_FOO.equals(action)) {
-                final String param1 = intent.getStringExtra(EXTRA_PARAM1);
-                final String param2 = intent.getStringExtra(EXTRA_PARAM2);
-                handleActionFoo(param1, param2);
-            } else if (ACTION_BAZ.equals(action)) {
-                final String param1 = intent.getStringExtra(EXTRA_PARAM1);
-                final String param2 = intent.getStringExtra(EXTRA_PARAM2);
-                handleActionBaz(param1, param2);
+            if (ACTION_RUN_TIMER.equals(action)) {
+                final String param1 = intent.getStringExtra(EXTRA_SECONDS);
+                handleActionFoo(param1);
             }
         }
     }
@@ -75,17 +52,9 @@ public class TimerService extends IntentService {
      * Handle action Foo in the provided background thread with the provided
      * parameters.
      */
-    private void handleActionFoo(String param1, String param2) {
+    private void handleActionFoo(String param1) {
         // TODO: Handle action Foo
         throw new UnsupportedOperationException("Not yet implemented");
     }
 
-    /**
-     * Handle action Baz in the provided background thread with the provided
-     * parameters.
-     */
-    private void handleActionBaz(String param1, String param2) {
-        // TODO: Handle action Baz
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
 }
